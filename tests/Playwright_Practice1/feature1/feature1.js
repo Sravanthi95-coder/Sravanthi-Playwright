350bdc1d641773163fcb5f690154557fafa642bc
Sravanthi <sravanthich2021@gmail.com>
2025-08-08 20:27:02 +0530

    conflict_removed

--- tests/Playwright_Practice1/feature1/feature1.js
+++ tests/Playwright_Practice1/feature1/feature1.js
@@ -1,6 +1,2 @@
-<<<<<<< HEAD
+
 console.log("Branching Practice_conflict2");
-=======
-console.log("Branching Practice_conflict2");
->>>>>>> conflict
-console.log("commit more changes in feature branch");
